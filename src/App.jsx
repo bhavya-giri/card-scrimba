677d06c4be645363925e1a672939d761262d80de
Bhavya Giri <bhavya.giri44@gmail.com>
2022-08-25 10:25:06 +0530

    pic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Top from './components/Top'
+import Bottom from './components/Bottom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+     <Top/>
+     <Bottom/>
     </div>
   )
 }
---
+++ src/components/Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import pic from '../assets/Rectangle 90.png'
+const Top = () => {
+  return (
+    <div>
+      <img className='pic' src={pic}/>
+    </div>
+  )
+}
+
+export default Top
